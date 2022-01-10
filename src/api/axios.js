2d651cf4bebde668a4042336005a7189c80995ad
unknown <husein.18@students.amikom.ac.id>
2022-01-10 15:52:55 +0700

    husein

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ var parts = location.hostname.split('.');
 // var subdomain = parts.shift();
 var upperleveldomain = parts.join('.');
 export default axios.create({
-  baseURL: 'http://127.0.0.1:8000/api'
+  baseURL: window.location.origin+'/public/api'
   // baseURL: 'https://1571156cb4fc.ngrok.io'
    // baseURL: 'https://akutansi456.herokuapp.com/api'
 })
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -20,7 +20,7 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
