6f6746314631738ba349ad3239acf8ee55b05b90
unknown <husein.18@students.amikom.ac.id>
2022-01-10 14:11:37 +0700

    husein

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 // 'https://cors-anywhere.herokuapp.com/apisiakad.hilmimubarok.com'
+var parts = location.hostname.split('.');
+// var subdomain = parts.shift();
+var upperleveldomain = parts.join('.');
 export default axios.create({
-  baseURL: 'https://akuntansiv2.autoresponder.my.id/api'
+  baseURL: 'http://127.0.0.1:8000/api'
   // baseURL: 'https://1571156cb4fc.ngrok.io'
    // baseURL: 'https://akutansi456.herokuapp.com/api'
-
-
 })
